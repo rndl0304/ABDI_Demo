3e455fbffd3f19b1ccfa9ea7c0aaec15a30d058e
rndl0304 <rndl0304@gmail.com>
2025-08-06 14:00:00 -0300

    corrigindo o erro

--- my-app/src/Components/Icon.tsx
+++ my-app/src/Components/Icon.tsx
@@ -3,7 +3,10 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Icon() {
   return (
-
+        <div className="chatbot-header">
+            <i className="fas fa-leaf chatbot-icon"></i>
+            <span>AgroBOT</span>
+        </div>
   );
 }
 
